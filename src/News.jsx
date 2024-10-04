c2620fcd30de3ec206488fb3db6dd1a41a220518
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-04 18:00:33 +0530

    Heading added

--- src/News.jsx
+++ src/News.jsx
@@ -2,34 +2,36 @@ import React from 'react';
 import Cards from './Cards';
 
 const News = () => {
-  return (
-    <div>
-      <nav>
+    return (
         <div>
-          <h1>TOP 100 NEWS</h1>
-        </div>
-        <ul>
-          <li><a href="#">All News</a></li>
-          <li><a href="#">Trending...</a></li>
-        </ul>
-        <div className="searchBar">
-          <input type="text" placeholder="Search news" />
-          <button>Search</button>
-        </div>
-      </nav>
+            <nav>
+                <div>
+                    <h1>TOP 100 NEWS</h1>
+                </div>
+                <ul>
+                    <li><a href="#">All News</a></li>
+                    <li><a href="#">Trending...</a></li>
+                </ul>
+                <div className="searchBar">
+                    <input type="text" placeholder="Search news" />
+                    <button>Search</button>
+                </div>
+            </nav>
 
-      <div className="category-btn">
-        <button>Sports</button>
-        <button>Global</button>
-        <button>Politics</button>
-        <button>Health</button>
-      </div>
+            <p className='top-heading'>Stay update with trending News</p>
 
-      <div>
-        <Cards />
-      </div>
-    </div>
-  );
+            <div className="category-btn">
+                <button>Sports</button>
+                <button>Global</button>
+                <button>Politics</button>
+                <button>Health</button>
+            </div>
+
+            <div>
+                <Cards />
+            </div>
+        </div>
+    );
 };
 
 export default News;
