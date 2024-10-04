d310ad7a7db27f4aabc645b4003b09e33b24078d
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-04 20:39:24 +0530

    Add search handler

--- src/News.jsx
+++ src/News.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cards from './Cards';
 
 const News = () => {
+
+    // to handle search 
+    const [search,setSearch]=useState("india")
 
 
     const API_KEY = "be8ba853b90042a38d21764254e2a8e8"
 
     const getData = async () => {
-        const response = await fetch(`https://newsapi.org/v2/everything?q=india&apiKey=${API_KEY}`)
+        const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`)
 
         const jsonData = await response.json();
         console.log(jsonData)
+
+    }
+
+    const handleSearch=(e)=>{
+        console.log(e.target.value);
+        setSearch(e.target.value)
 
     }
 
@@ -28,7 +37,7 @@ const News = () => {
                     <li><a href="#">Trending...</a></li>
                 </ul>
                 <div className="searchBar">
-                    <input type="text" placeholder="Search news" />
+                    <input type="text" placeholder="Search news" onChange={handleSearch}/>
                     <button onClick={getData}>Search</button>
                 </div>
             </nav>
