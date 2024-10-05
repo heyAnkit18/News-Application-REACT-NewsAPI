19bd75588a855c492ede01e1478940ef206c652b
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-05 12:07:14 +0530

    Deploy error Removed

--- src/News.jsx
+++ src/News.jsx
@@ -1,44 +1,40 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import Cards from './Cards';
 
 const News = () => {
-    // to handle search 
     const [search, setSearch] = useState("india");
     const [newsData, setNewsData] = useState(null);
     const API_KEY = "be8ba853b90042a38d21764254e2a8e8";
 
-    const getData = async () => {
+    // Wrapping getData with useCallback to avoid re-creating the function on every render
+    const getData = useCallback(async () => {
         const response = await fetch(`https://newsapi.org/v2/everything?q=${search}&apiKey=${API_KEY}`);
         const jsonData = await response.json();
-        console.log(jsonData.articles);
         setNewsData(jsonData.articles);
-    }
+    }, [search, API_KEY]);
 
     const handleSearch = (e) => {
-        console.log(e.target.value);
         setSearch(e.target.value);
     }
 
-    // to get initial data on home page 
+    // Adding getData as a dependency in useEffect
     useEffect(() => {
         getData();
-    }, []);
+    }, [getData]);
 
-    // Function to handle category button clicks
     const handleCategoryClick = (category) => {
-        setSearch(category); // Set search term to the category
-        getData(); // Fetch news data for the new category
+        setSearch(category);
+        getData();
     }
 
     return (
         <div>
-            <nav>
+            <nav className='nav'>
                 <div>
                     <h1>Breaking News</h1>
                 </div>
                 <ul>
-                    <li><a href="#">All News</a></li>
-                    <li><a href="#">Trending...</a></li>
+                    <li>Welcome to Breaking News – Your Ultimate Source for the Latest Headlines!</li>
                 </ul>
                 <div className="searchBar">
                     <input type="text" placeholder="Search news" value={search} onChange={handleSearch} />
@@ -66,4 +62,5 @@ const News = () => {
 };
 
 export default News;
+
 
