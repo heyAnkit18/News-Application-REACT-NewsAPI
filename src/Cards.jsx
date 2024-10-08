b60f1b3770709c5c7d51d46894051f24a9c577e1
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-09 00:10:29 +0530

    Cleaning code

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -3,25 +3,23 @@ import React from 'react';
 const Cards = ({ data }) => {
   console.log(data);
 
-  // Check if data exists and is not null before trying to map it
+ 
   if (!data || data.length === 0) {
     return <p className='no-news'>Oops.. No news available!</p>;
   }
-
-  // Read more functionality with window.open method
   const readMore = (url) => {
     window.open(url);
   };
 
   return (
     <div className='cards'>
-      {/* Iterate over the data */}
+
       {data.map((item, index) => {
-        // Check if urlToImage exists
+
         if (!item.urlToImage) {
-          return null; 
+          return null;
         }
-        
+
         return (
           <div className='card' key={index}>
             <img src={item.urlToImage} alt={item.title} />
@@ -30,7 +28,6 @@ const Cards = ({ data }) => {
                 {item.title}
               </a>
               <p>{item.description}</p>
-              {/* Corrected the onClick event */}
               <button onClick={() => readMore(item.url)}>Read More</button>
             </div>
           </div>
